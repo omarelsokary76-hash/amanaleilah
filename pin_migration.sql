5019d95b550d2e021b72ddc52d7586f0ad1ffba2
Akhtyaraty <omarelsokary76@gmail.com>
2026-09-16 02:32:26 +0300

    Delete pin_migration.sql

--- pin_migration.sql
+++
@@ -1,5 +0,0 @@
--- شغّل الملف ده مرة واحدة على قاعدة البيانات الموجودة عندك بالفعل، عشان يضيف دعم
--- الدخول بالـ PIN بدل رقم الهاتف (رقم الهاتف هيفضل موجود لبيانات التواصل بس).
-
-ALTER TABLE members ADD COLUMN pin_hash TEXT;
-ALTER TABLE members ADD COLUMN recovery_code_hash TEXT;
