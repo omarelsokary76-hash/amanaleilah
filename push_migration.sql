4197045bfdd908068cb38ae367e482f6e223b37f
Akhtyaraty <omarelsokary76@gmail.com>
2026-09-21 10:56:39 +0300

    Delete push_migration.sql

--- push_migration.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS push_subscriptions (
-  member_id TEXT PRIMARY KEY,
-  family_code TEXT NOT NULL,
-  endpoint TEXT NOT NULL,
-  p256dh TEXT NOT NULL,
-  auth TEXT NOT NULL,
-  created_at INTEGER NOT NULL
-);
